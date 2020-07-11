f6d8b792c20e958f7e5ba8b94d9d351dffc3fe78
Eric <ericbuxkemper@gmail.com>
2020-07-11 14:22:58 -0500

    Added pause menu

---
+++ Assets/Scripts/GameFlow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFlow : MonoBehaviour
+{
+    public int test;
+    public Texture rose;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(rose.name);
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

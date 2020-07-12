60da32ea5bce753cb20b813f77e9150ac01e43d0
Taylor Buxkemper <taylor.buxkemper15@gmail.com>
2020-07-11 21:07:58 -0500

    grass bug

--- Assets/Scripts/GrassManager.cs
+++ Assets/Scripts/GrassManager.cs
@@ -6,7 +6,16 @@ public class GrassManager : MonoBehaviour {
     public List<GameObject> spawnPoints;
     public List<GameObject> grassPrefabs;
 
-    public void SpawnGrass() {
+    public void SpawnGrass()
+    {
+        var spawnPoint = spawnPoints[0];
+        var grass = Instantiate(grassPrefabs[0]);
+        grass.transform.position = spawnPoint.transform.position;
+    }
+
+    void Update()
+    {
+        SpawnGrass();
         
     }
 }

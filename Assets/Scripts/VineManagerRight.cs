e8a079d458df822fee91e87c9a388da1fa1f0fd0
Taylor Buxkemper <taylor.buxkemper15@gmail.com>
2020-07-12 09:49:35 -0500

    Vine Manager scripts created

---
+++ Assets/Scripts/VineManagerRight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VineManagerRight : SingletonBehaviour<VineManagerRight>{
+
+    public List<GameObject> vineSpawnPoints;
+    public List<GameObject> vinePrefabs;
+
+    public void SpawnVines()
+    {
+        var vineSpawnPoint = vineSpawnPoints[Random.Range(0, 4)];
+        var vines = Instantiate(vinePrefabs[Random.Range(0, 2)]);
+        vines.transform.SetParent(transform);
+        vines.transform.position = vineSpawnPoint.transform.position;
+    }
+}
+

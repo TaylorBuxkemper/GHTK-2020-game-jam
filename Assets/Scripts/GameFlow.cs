1a10f77d36b9f19593f06cea82aa6e3184435e1e
Eric <ericbuxkemper@gmail.com>
2020-07-11 20:40:27 -0500

    Adding gameflow code

--- Assets/Scripts/GameFlow.cs
+++ Assets/Scripts/GameFlow.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameFlow : MonoBehaviour {
     public float startDelay = 6f;
+    public float gameSpeed = 1f;
     public int test;
     public Texture rose;
     
@@ -19,17 +20,42 @@ public class GameFlow : MonoBehaviour {
         // start game with bud on a stem
         
         yield return new WaitForSeconds(startDelay);
+        StartLevel(1);
         // transition to pink peeking through bud
         
         yield return new WaitForSeconds(15);
+        StartLevel(2);
         // transition to bud peeling off rose flower
 
         yield return new WaitForSeconds(25);
+        StartLevel(3);
         // transition to bud completely off 
 
         yield return new WaitForSeconds(35);
+        StartLevel(4);
         // transition to rose flower poofed up
     }
-    
-    
+
+    private void StartLevel(int level) {
+        switch (level) {
+            case 1:
+                gameSpeed = 1.25f;
+                break;
+            case 2:
+                gameSpeed = 1.5f;
+                
+                break;
+            case 3:
+                gameSpeed = 2f;
+                break;
+            case 4:
+                StartCoroutine(ContinuouslyIncreaseGameSpeed());
+        }
+    }
+
+    private IEnumerator ContinuouslyIncreaseGameSpeed() {
+        while (Rose.I.isAlive) {
+            gameSpeed += .01f;
+        }
+    }
 }
